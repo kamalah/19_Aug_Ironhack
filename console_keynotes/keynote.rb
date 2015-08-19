8abb75bb9d8212e5446174b96c7a1770c5aeb913
kamalah <kamalah.chang@gmail.com>
2015-08-19 21:46:32 +0200

    moved show_slide to slide class

--- console_keynotes/keynote.rb
+++ console_keynotes/keynote.rb
@@ -6,7 +6,7 @@ class Slides
 	attr_accessor :all_slides
 	def initialize
 		@all_slides =[""]
-
+		@size = TermInfo.screen_size
 	end
 
 	def get_slides(file_name)
@@ -28,6 +28,36 @@ class Slides
 	def number_slides
 		@all_slides.length
 	end
+
+	def show_slide(ind)
+		slide = @all_slides[ind]
+		center_vertical(slide)
+		find_endl = 0
+		print_slide = slide
+
+		while(find_endl <print_slide.length) #print line by line
+			center_right(slide)
+			find_endl = print_slide.index("\n")
+			puts print_slide[0..find_endl]
+			print_slide = print_slide[find_endl+1..print_slide.length]
+		end
+		center_vertical(slide)	
+	end
+
+	def center_vertical(slide)
+		above_lines = (@size[0] - slide.count("\n"))/2
+		above_lines.times do
+			puts "\n"
+			end
+	end
+
+	def center_right(slide)
+		spaces = (@size[1] - slide.index("\n"))/2
+		spaces.times do
+			print " "
+			end
+	end
+
 end
 
 class Presentation
@@ -36,11 +66,11 @@ class Presentation
 	def initialize(slides)
 		@slides = slides
 		@location = 0
-		@size = TermInfo.screen_size
+		
 	end
 
 	def start_show
-		show_slide(@slides.send_slide(0))
+		show_slide(0)
 		while @location !=-1
 			check_input
 		end
@@ -69,14 +99,14 @@ class Presentation
 	def next_slide
 		if @location < (@slides.number_slides-1)
 			@location += 1
-			show_slide(@slides.send_slide(@location))
+			show_slide(@location)
 		end
 	end
 
 	def previous_slide
 		if @location > 0
 			@location -= 1
-			show_slide(@slides.send_slide(@location))
+			show_slide(@location)
 		end
 	end
 
@@ -88,36 +118,12 @@ class Presentation
 	end
 
 
-	def show_slide (slide)
+	def show_slide (ind)
 		system "clear"
-		center_vertical(slide)
-
-		find_endl = 0
-		print_slide = slide
-
-		while(find_endl <print_slide.length) #print line by line
-			center_right(slide)
-			find_endl = print_slide.index("\n")
-			puts print_slide[0..find_endl]
-			print_slide = print_slide[find_endl+1..print_slide.length]
-		end
-
-		center_vertical(slide)	
+		@slides.show_slide(ind)
 	end
 
-	def center_vertical(slide)
-		above_lines = (@size[0] - slide.count("\n"))/2
-		above_lines.times do
-			puts "\n"
-			end
-	end
-
-	def center_right(slide)
-		spaces = (@size[1] - slide.index("\n"))/2
-		spaces.times do
-			print " "
-			end
-	end
+	
 end
 
 
