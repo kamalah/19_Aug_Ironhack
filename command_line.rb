fa13b26f33a2d877e9424dbba62af73975c9ac6c
kamalah <kamalah.chang@gmail.com>
2015-08-19 11:06:55 +0200

    added menu option

--- command_line.rb
+++ command_line.rb
@@ -1,10 +1,12 @@
 class CommandLine
-	def initialize (name ="Alex", password="1234")
-		@user_name = name
-		@hard_password = password
+	def initialize 
+		@user_name = "Alex"
+		@hard_password = "1234"
 		@correct = false
 		@enter_user = ""
 		@enter_password = ""
+		@text = ""
+		@option = ""
 	end
 
 	def get_entry
@@ -26,22 +28,45 @@ class CommandLine
 	def get_text
 		if @correct
 			print "Please enter text: "
-			text = gets.chomp
-			text_array = text.split
-			puts "There are #{text_array.length} words."
+			@text = gets.chomp
+			
 		end
-		
+
+	end
+
+	def show_options
+		if @correct
+			puts "Select option below"
+			puts "1. Count words"
+			puts "2. Count letter"
+			puts "3. Reverse text"
+			puts "4. Uppercase"
+			puts "5. Lowercase"
+
+			@options = gets.chomp
+
+			case @options
+			when "1"
+				text_array = @text.split
+				puts "There are #{text_array.length} words."
+			when "2"
+				@text = @text.sub(" ", "")
+				 puts "There are #{@text.length} letters."
+			when "3"
+				puts "The reverse is #{@text.reverse}"
+			when "4"
+				puts "The Uppercase is #{@text.upcase}"
+			when "5"
+				puts "The Lowercase is #{@text.downcase}"
+			else
+				puts "Not a valid option."	
+			end
+		end
 	end
 end
-
 
 my_command = CommandLine.new
 my_command.get_entry
 my_command.check_entry
 my_command.get_text
-
-
-other_command = CommandLine.new("Kamalah", "2345")
-other_command.get_entry
-other_command.check_entry
-other_command.get_text
+my_command.show_options
